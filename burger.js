6b8f10bfbf844b7d6e86776e9bbd11212ab1a11e
MaksBugovskiy <dj.maksimus@gmail.com>
2017-03-28 21:14:43 +0300

    Завдання з бургерами

--- burger.js
+++ burger.js
@@ -1,16 +1,9 @@
-class BigBurger {
-    constructor () {
-        this.price = 100;
-        this.cal = 40;
-    }
-};
- 
- class SmBurger {
-     constructor () {
-         this.price = 50;
-         this.cal = 20;
-     }
- }
+const bigburgerprice = 100;
+const bigburgercal = 40;
+const smburgerprice = 50;
+const smburgercal = 20;
+
+
  class IngPrice {
       salat () {
           return this.price = 10;
@@ -41,10 +34,11 @@ class IngCal{
       }
  };
 
-var burgerbig = new BigBurger();
-var burgersm = new SmBurger();
 var priceing = new IngPrice();
 var caling = new IngCal();
 
 
-console.log(burgerbig.price + priceing.cheese() + priceing.meat() + priceing.salat());
+console.log('Ціна великого бургера', + bigburgerprice + priceing.cheese() + priceing.meat() + priceing.salat() + priceing.mayo());
+console.log('Ціна маленького бургера', + smburgerprice + priceing.cheese() + priceing.mayo() + priceing.meat() + priceing.salat());
+console.log( 'Калорійність великого бургера', + bigburgercal + caling.salat() + caling.cheese() + caling.mayo() + caling.meat());
+console.log( 'Калорійність маленького бургера', + smburgercal + caling.salat() + caling.cheese() + caling.mayo() + caling.meat());
